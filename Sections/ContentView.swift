52db989feda2f6ac5e4e0866fbac7ef2132333da
Wilhelm Shakely Fors <wilhelm.fors@caspeco.se>
2026-06-25 14:18:17 +0200

    Initial Commit

---
+++ Sections/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sections
+//
+//  Created by Wilhelm Shakely Fors on 2026-06-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sections/SectionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SectionsApp.swift
+//  Sections
+//
+//  Created by Wilhelm Shakely Fors on 2026-06-25.
+//
+
+import SwiftUI
+
+@main
+struct SectionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
